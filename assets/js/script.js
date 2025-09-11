71a78c8509594182281695275d6b6d92324a202a
Xxmaxiolivares777xX <maxi.olivares@duocuc.cl>
2025-09-11 18:21:44 -0300

    avers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,47 @@
 //api
-const pokemon = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+let pokemon = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+let favoritos =["oshawott", "leavanny", "pikachu", "haxorus", "feraligatr", "dragonite"]
 async function llamarPokemon() {
-    try{
-        const llamado = await fetch(pokemon);
-        if (!llamado.ok){
-            throw new Error ("Pero nadie vino")
+    let contenedor = document.getElementById("pokemon");
+    for (let i = 0; i < favoritos.length; i++) {
+        let nombre= favoritos[i]
+        try {
+          let llamado = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+          let data = await llamado.json();
+          let tipos = data.types.map(t => t.type.name).join(", ");
+          contenedor.innerHTML += `
+            <div class="col-12 col-sm-6 mb-5">
+          <div class="card text-center shadow-sm h-100">
+            <img src="${data.sprites.front_default}" class="card-img-top p-1">
+            <div class="card-body">
+              <h2 class="card-title">${data.name}</h2>
+              <h5 class="card-text mb-0"><strong>Tipos:</strong> ${tipos}</h5>
+            </div>
+          </div>
+        </div>`;
+        } catch (error) {
+          console.error(`Se te a escapado ${nombre}`, error);
         }
-        const paseo = await llamado.json();
-        console.log(paseo.results)
-        for (let i = 0; i < paseo.results.length; i++){
-            console.log(paseo.results[i].name);
-            let cadaUno = document.createElement("div");
-            cadaUno.classList.add('col-md-6')
-            cadaUno.innerHTML = '<div class= "card mb-5 text-center"><img src"' + paseo.results[i].sprites + '" class=" w-100">' + "<h3>" + paseo.results[i].name + "</h3></div>"
-            document.getElementById('pokemon').appendChild(cadaUno)
-        }
-    }catch(error){
-        console.error('Hay un problema o hiciste algo mal', error);
+      }
     }
-}
-//Si llamo una api de varios pokemon, solo me da el nombre. Hago uno individual aparte para llamar mas datos.
-llamarPokemon();
-const pokeFavorito= "https://pokeapi.co/api/v2/pokemon/krokorok"
+
+    llamarPokemon();
+
+let pokeFavorito= "https://pokeapi.co/api/v2/pokemon/krokorok"
 async function llamarAlPokemon(){
     try{
-        const llamada = await fetch(pokeFavorito);
+        let llamada = await fetch(pokeFavorito);
         if(!llamada.ok){
             throw new Error ("No te quiere")
         }
-        const uno = await llamada.json();
-        const tipos = uno.types.map(t => t.type.name).join(", ");
-        document.getElementById("pokeFav").innerHTML= "<div class='col-6 m-0 m-auto card text-center'>" +
+        let uno = await llamada.json();
+        let tipos = uno.types.map(t => t.type.name).join(", ");
+        document.getElementById("pokeFav").innerHTML= 
+            "<div class='col-6 m-0 m-auto card text-center'>" +
             "<img src='" + uno.sprites.front_default + "' class= 'w-100'>" +
-            "<h3>" + uno.name + "</h3>" +
-            "Tipos: " + tipos +
-            "</div>"; 
+            "<div='card-body'><h2>" + uno.name + "</h2>" +
+            "<h5><strong>Tipos:</strong> " + tipos +
+            "</h5></div></div>"; 
         
     }catch(error){
         console.error('Hay un problema o hiciste algo mal 2', error);
